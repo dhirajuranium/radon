64a74597ca6962c7bafa15b8a05990fa48681c62
dhirajuranium <dhirajpatil2311@gmail.com>
2022-06-02 10:28:15 +0530

    day 13

---
+++ src/logger/logger.js
@@ -0,0 +1,28 @@
+let abc = function() {
+    console.log("Welcome to my application. I am dhiraj and a part of FunctionUp Radon cohort.")
+}
+
+module.exports.abc = abc
+
+
+
+
+
+
+
+
+
+
+
+
+// ========================================================== SABHIHA MAAM TEACHING CODE 01/06/2022 DAY 13 =========================================
+// const endpoint = "https://www.functionup.org"
+// const batch = "radon"
+
+// const log = function() {
+//     console.log('I am inside the log function')
+// }
+
+// module.exports.endpoint = endpoint
+// module.exports.batch = batch
+// module.exports.log = log
---
+++ src/validator/formatter.js
@@ -0,0 +1,16 @@
+let trim = function(){
+    console.log('The result after trimming the string is :' + ' functionUp  batch  '.trim() + '.')
+ 
+}
+
+let changetoLowerCase = function(){
+    console.log("The result after using toLowerCase function is :","DhIraj".toLowerCase())
+}
+
+let changeToUpperCase = function() {
+    console.log("The result after using toUpperCase function is", "raDon".toUpperCase())
+}
+
+module.exports.trimString = trim
+module.exports.changeCaseToLower = changetoLowerCase
+module.exports.changeCaseToUpper = changeToUpperCase
