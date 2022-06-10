aa79d1e435762b0a2345a80353dea2e89382cf0f
dhirajuranium <dhirajpatil2311@gmail.com>
2022-06-10 21:02:30 +0530

    Day 20

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+// const { default: mongoose } = require('mongoose');
 const app = express();
+const Middleware = require("../src/middlewares/commonMiddlewares")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+// mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+//     useNewUrlParser: true
+// })
+// .then( () => console.log("MongoDb is connected"))
+// .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+
+app.use(Middleware.middle1)       //<======= THIS IS GLOBAL MIDDLEWARE 
+
 
 app.use('/', route);
 
