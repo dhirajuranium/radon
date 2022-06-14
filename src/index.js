7d80e0d981f43d8a36b282399068eb1506a67f4c
dhirajuranium <dhirajpatil2311@gmail.com>
2022-06-14 22:58:36 +0530

    Day 21

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://dhirajpatil:XuEAzywgRheQB7B1@cluster0.0v32f.mongodb.net/Dhiraj2198-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const Auth = require("../Middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,8 +12,10 @@ router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",Auth.authMiddleware, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",Auth.authMiddleware, userController.updateUser)
+
+router.delete("/delet-users/:userId",Auth.authMiddleware, userController.deleteUser)
 
 module.exports = router;
